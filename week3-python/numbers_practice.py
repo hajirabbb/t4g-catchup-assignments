5b95f5cc61d30252412475a7abcd56e22e429d4d
hajirabbb <hajirabaffoe@gmail.com>
2026-03-05 12:10:35 +0000

    part 3 of assignment

---
+++ week3-python/numbers_practice.py
@@ -0,0 +1,69 @@
+#Task 1
+first_int_value = 20
+second_int_value = 7
+first_float_value = 4.5
+second_float_value = 6.2
+
+#adding the integer values
+addition = first_int_value + second_int_value
+ 
+#subtracting the integer values
+subtraction = first_int_value - second_int_value
+
+
+#multiplying integer values
+multiplication = first_int_value  * second_int_value
+
+#dividing integer values
+division = first_int_value / second_int_value
+
+# floor dividing integer values
+floor_division = first_int_value // second_int_value
+
+#modulo of interger values
+modulo = first_int_value % second_int_value
+
+#exponentiation on integer value
+exponentiation = first_int_value ** second_int_value
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(floor_division)
+print(modulo)
+print(exponentiation)
+
+#Task 3
+
+#Declaring variables
+length = 14
+width = 20
+
+#finding area
+ 
+area = length * width
+
+# finding perimeter
+perimeter = 2 * length + 2 *width
+
+results = f"A triangle with length {length} cm and width {width} cm has an area of {area} cm^2 and a perimeter of {perimeter} cm"
+print(results)
+
+
+number1 = 34
+number2 = 45
+float(number1)
+float(number2)
+
+decimal1 = 4.5
+decimal2 = 5.7
+int(decimal1)
+int(decimal2)
+
+print(type(number1))
+print(type(number2))
+print(type(decimal1))
+print(type(decimal2))
+
+
