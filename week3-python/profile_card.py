efb1014183f6667154871be23c3f96c7f8bc1048
hajirabbb <hajirabaffoe@gmail.com>
2026-03-06 17:06:21 +0000

    submitting part 4 of assignment

---
+++ week3-python/profile_card.py
@@ -0,0 +1,25 @@
+name = "hajira baffoe"
+age = 21
+cohort = 4
+track = "Backend"
+current_week = 4
+favorite_topic = "Bash Scripting"
+name.title()
+favorite_topic.replace("Bash", "shell")
+print("=" * 38)
+print("STUDENT PROFILE")
+print("=" * 38)
+print("Name : {name}")
+print("Age : {age}")
+print("Class: {track}")
+print("Week : {current_week}")
+print("Favorite Topic: {favorite_topic}")
+print("=" * 38)
+#footer
+total_weeks = 12
+Weeks_left = total_weeks - current_week
+print(f"Weeks left is {Weeks_left}")
+print("=" * 38)
+
+
+
